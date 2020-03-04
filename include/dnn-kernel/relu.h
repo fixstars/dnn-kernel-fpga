1a9138cc323c5d8a224de1cea3848880e3288975
Akira Maruoka <akmaru0266@gmail.com>
2020-03-04 23:44:39 +0900

    Fixed include gurads

--- include/dnn-kernel/relu.h
+++ include/dnn-kernel/relu.h
@@ -1,5 +1,5 @@
-#ifndef DNNTEST_RELU_H
-#define DNNTEST_RELU_H
+#ifndef DNNKERNEL_RELU_H
+#define DNNKERNEL_RELU_H
 
 #include <stdint.h>
 #include <algorithm>
@@ -16,4 +16,4 @@ void relu(const float *x, int64_t size, float *y) {
 }  // namespace
 }  // namespace dnnk
 
-#endif  // DNNTEST_RELU_H
+#endif  // DNNKERNEL_RELU_H
