6baf947d0e07dbfc90bbb5d2874ce77aa719613a
yuki.matsuda <yuki.matsuda@fixstars.com>
2020-06-23 17:01:07 +0900

    implement linear layer.

---
+++ include/dnn-kernel/linear.h
@@ -0,0 +1,23 @@
+#ifndef DNNKERNEL_RELU_H
+#define DNNKERNEL_RELU_H
+
+#include <stdint.h>
+#include <algorithm>
+
+namespace dnnk {
+namespace {
+
+void linear(const float *x, const float* weight, const float* bias, int64_t in_features, int64_t out_features, float *y) {
+    for (int64_t i = 0; i < out_features; ++i) {
+        float sum = 0.f;
+        for (int64_t j = 0; j < in_features; ++j) {
+            sum += x[j] * weight[i * in_features + j];
+        }
+        y[i] = sum + bias[i];
+    }
+}
+
+}  // namespace
+}  // namespace dnnk
+
+#endif  // DNNKERNEL_RELU_H
---
+++ tests/hls/linear/linear_hls.cc
@@ -0,0 +1,15 @@
+#include "dnn-kernel/linear.h"
+
+#include <stdint.h>
+#include <algorithm>
+
+static const std::size_t kMaxSize = 65536;
+
+void linear_hls(const float x[kMaxSize], const float weight[kMaxSize], const float bias[kMaxSize], int32_t in_features, int32_t out_features, float y[kMaxSize]) {
+#pragma HLS dataflow
+#pragma HLS interface s_axilite port = return bundle = BUS_AXI4LS
+#pragma HLS interface s_axilite port = in_features bundle = BUS_AXI4LS
+#pragma HLS interface s_axilite port = out_features bundle = BUS_AXI4LS
+
+    dnnk::linear(x, weight, bias, in_features, out_features, y);
+}
