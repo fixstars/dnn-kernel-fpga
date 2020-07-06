c9f1c90bae76f3fce021c067d3d3a9c92785fe0d
yuki.matsuda <yuki.matsuda@fixstars.com>
2020-07-06 21:37:21 +0900

    remove gpu function, and increase batch size to reduce learning time.

--- learning/train_mnist.py
+++ learning/train_mnist.py
@@ -60,21 +60,17 @@ class Net(nn.Module):
         return x
 
 
+net = Net()
+
 # 2. データセットの読み出し法の定義
 # MNIST の学習・テストデータの取得
 trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transforms.ToTensor())
 testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transforms.ToTensor())
 
 # データの読み出し方法の定義
-# 1stepの学習・テストごとに4枚ずつ画像を読みだす
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle=True)
-testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=False)
-
-# 使用デバイス自動定義(CUDAが使えればCUDA、そうでないときはCPU)
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-net = Net()
-net = net.to(device)
+# 1stepの学習・テストごとに16枚ずつ画像を読みだす
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=16, shuffle=True)
+testloader = torch.utils.data.DataLoader(testset, batch_size=16, shuffle=False)
 
 # ロス関数、最適化器の定義
 loss_func = nn.CrossEntropyLoss()
@@ -91,8 +87,6 @@ for epoch in range(10):
     for i, data in enumerate(trainloader, 0):
         # 入力バッチの読み込み (画像、正解ラベル)
         inputs, labels = data
-        inputs = inputs.to(device)
-        labels = labels.to(device)
 
         # 最適化器をゼロ初期化
         optimizer.zero_grad()
@@ -107,9 +101,9 @@ for epoch in range(10):
         # 誤差を用いてモデルの最適化
         optimizer.step()
         running_loss += loss.item()
-        if i % 2000 == 1999:
+        if i % 1000 == 999:
             print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
+                  (epoch + 1, i + 1, running_loss / 1000))
             running_loss = 0.0
 print('end training:')
 
@@ -118,11 +112,8 @@ ans = []
 pred = []
 for i, data in enumerate(testloader, 0):
     inputs, labels = data
-    inputs = inputs.to(device)
-    labels = labels.to(device)
 
     outputs = net(inputs)
-    outputs = outputs.to('cpu')
 
     ans += labels.tolist()
     pred += torch.argmax(outputs, 1).tolist()
