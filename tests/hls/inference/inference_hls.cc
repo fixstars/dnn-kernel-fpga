0b1b00855f2eb9a780e37a202ba571b374ced67c
yuki.matsuda <yuki.matsuda@fixstars.com>
2020-07-17 05:00:16 +0900

    use `v++ -compile` instead of Vivado HLS.

--- tests/hls/inference/inference_hls.cc
+++ tests/hls/inference/inference_hls.cc
@@ -11,9 +11,6 @@ void inference_hls(const float x[kMaxSize],
                    const float weight2[kMaxSize], const float bias2[kMaxSize],
                    const float weight3[kMaxSize], const float bias3[kMaxSize],
                    float y[kMaxSize]) {
-#pragma HLS dataflow
-#pragma HLS interface s_axilite port = return bundle = BUS_AXI4LS
-
   dnnk::inference<1, 4, 8, 32, 10>(x,
                                    weight0, bias0,
                                    weight1, bias1,
@@ -30,18 +27,17 @@ void inference_top(const float x[kMaxSize],
                    const float weight2[kMaxSize], const float bias2[kMaxSize],
                    const float weight3[kMaxSize], const float bias3[kMaxSize],
                    float y[kMaxSize]) {
-#pragma HLS dataflow
-#pragma HLS interface s_axilite port = return bundle = control
-#pragma HLS interface m_axi port = x bundle = gmem0
-#pragma HLS interface m_axi port = weight0 bundle = gmem1
-#pragma HLS interface m_axi port = weight1 bundle = gmem2
-#pragma HLS interface m_axi port = weight2 bundle = gmem3
-#pragma HLS interface m_axi port = weight3 bundle = gmem4
-#pragma HLS interface m_axi port = bias0 bundle = gmem5
-#pragma HLS interface m_axi port = bias1 bundle = gmem6
-#pragma HLS interface m_axi port = bias2 bundle = gmem7
-#pragma HLS interface m_axi port = bias3 bundle = gmem8
-#pragma HLS interface m_axi port = y bundle = gmem9
+#pragma HLS interface s_axilite port=return bundle=control
+#pragma HLS interface m_axi port=x offset=slave bundle=gmem0
+#pragma HLS interface m_axi port=weight0 offset=slave bundle=gmem1
+#pragma HLS interface m_axi port=weight1 offset=slave bundle=gmem2
+#pragma HLS interface m_axi port=weight2 offset=slave bundle=gmem3
+#pragma HLS interface m_axi port=weight3 offset=slave bundle=gmem4
+#pragma HLS interface m_axi port=bias0 offset=slave bundle=gmem5
+#pragma HLS interface m_axi port=bias1 offset=slave bundle=gmem6
+#pragma HLS interface m_axi port=bias2 offset=slave bundle=gmem7
+#pragma HLS interface m_axi port=bias3 offset=slave bundle=gmem8
+#pragma HLS interface m_axi port=y offset=slave bundle=gmem9
 
   dnnk::inference<1, 4, 8, 32, 10>(x,
                                    weight0, bias0,
