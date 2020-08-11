4b1cbd7c483be15159cded6af81f6e121b96085a
yuki.matsuda <yuki.matsuda@fixstars.com>
2020-08-11 18:29:19 +0900

    add conv2d_pipelined func.

--- include/dnn-kernel/conv2d.h
+++ include/dnn-kernel/conv2d.h
@@ -41,6 +41,44 @@ static void conv2d(const float* x, const float* weight, const float* bias, int32
   }
 }
 
+
+static void conv2d_pipelined(const float* x, const float* weight, const float* bias, int32_t width, int32_t height,
+                             int32_t in_channels, int32_t out_channels, int32_t ksize, float* y) {
+  for (int32_t och = 0; och < out_channels; ++och) {
+    for (int32_t h = 0; h < height; ++h) {
+      for (int32_t w = 0; w < width; ++w) {
+        float sum = 0.f;
+
+        for (int32_t ich = 0; ich < in_channels; ++ich) {
+          for (int32_t kh = 0; kh < ksize; ++kh) {
+            for (int32_t kw = 0; kw < ksize; ++kw) {
+#pragma HLS pipeline II=1
+
+              int32_t ph = h + kh - ksize/2;
+              int32_t pw = w + kw - ksize/2;
+
+              // zero padding
+              if (ph < 0 || ph >= height || pw < 0 || pw >= width) {
+                continue;
+              }
+
+              int64_t pix_idx = (ich * height + ph) * width + pw;
+              int64_t weight_idx = ((och * in_channels + ich) * ksize + kh) * ksize + kw;
+
+              sum += x[pix_idx] * weight[weight_idx];
+            }
+          }
+        }
+
+        // add bias
+        sum += bias[och];
+
+        y[(och * height + h) * width + w] = sum;
+      }
+    }
+  }
+}
+
 }  // namespace dnnk
 
 #endif  // DNNKERNEL_CONV2D_H
--- include/dnn-kernel/inference.h
+++ include/dnn-kernel/inference.h
@@ -11,12 +11,18 @@
 
 namespace dnnk {
 
-static void inference(const float* x,
-                      const float* weight0, const float* bias0,
-                      const float* weight1, const float* bias1,
-                      const float* weight2, const float* bias2,
-                      const float* weight3, const float* bias3,
-                      float* y) {
+typedef void (*conv2d_t)(const float*, const float*, const float*, int32_t, int32_t, int32_t, int32_t, int32_t, float*);
+typedef void (*maxpool2d_t)(const float*, int32_t, int32_t, int32_t, int32_t, float*);
+typedef void (*relu_t)(const float*, int64_t, float*);
+typedef void (*linear_t)(const float*, const float*, const float*, int64_t, int64_t, float*);
+
+template <conv2d_t conv2d_f, maxpool2d_t maxpool2d_f, relu_t relu_f, linear_t linear_f>
+static void inference_custom(const float* x,
+                             const float* weight0, const float* bias0,
+                             const float* weight1, const float* bias1,
+                             const float* weight2, const float* bias2,
+                             const float* weight3, const float* bias3,
+                             float* y) {
 #pragma HLS inline
 
   static const int kWidths[] = {28, 14, 7};
@@ -33,21 +39,37 @@ static void inference(const float* x,
   float x8[kChannels[3]];
 
   // 1st layer
-  conv2d(x, weight0, bias0, kWidths[0], kHeights[0], kChannels[0], kChannels[1], 3, x1);
-  relu(x1, kWidths[0] * kHeights[0] * kChannels[1], x2);
-  maxpool2d(x2, kWidths[0], kHeights[0], kChannels[1], 2, x3);
+  conv2d_f(x, weight0, bias0, kWidths[0], kHeights[0], kChannels[0], kChannels[1], 3, x1);
+  relu_f(x1, kWidths[0] * kHeights[0] * kChannels[1], x2);
+  maxpool2d_f(x2, kWidths[0], kHeights[0], kChannels[1], 2, x3);
 
   // 2nd layer
-  conv2d(x3, weight1, bias1, kWidths[1], kHeights[1], kChannels[1], kChannels[2], 3, x4);
-  relu(x4, kWidths[1] * kHeights[1] * kChannels[2], x5);
-  maxpool2d(x5, kWidths[1], kHeights[1], kChannels[2], 2, x6);
+  conv2d_f(x3, weight1, bias1, kWidths[1], kHeights[1], kChannels[1], kChannels[2], 3, x4);
+  relu_f(x4, kWidths[1] * kHeights[1] * kChannels[2], x5);
+  maxpool2d_f(x5, kWidths[1], kHeights[1], kChannels[2], 2, x6);
 
   // 3rd layer
-  linear(x6, weight2, bias2, kWidths[2] * kHeights[2] * kChannels[2], kChannels[3], x7);
-  relu(x7, kChannels[3], x8);
+  linear_f(x6, weight2, bias2, kWidths[2] * kHeights[2] * kChannels[2], kChannels[3], x7);
+  relu_f(x7, kChannels[3], x8);
 
   // 4th layer
-  linear(x8, weight3, bias3, kChannels[3], kChannels[4], y);
+  linear_f(x8, weight3, bias3, kChannels[3], kChannels[4], y);
+}
+
+static void inference(const float* x,
+                      const float* weight0, const float* bias0,
+                      const float* weight1, const float* bias1,
+                      const float* weight2, const float* bias2,
+                      const float* weight3, const float* bias3,
+                      float* y) {
+#pragma HLS inline
+
+  inference_custom<conv2d, maxpool2d, relu, linear>(x,
+                                                    weight0, bias0,
+                                                    weight1, bias1,
+                                                    weight2, bias2,
+                                                    weight3, bias3,
+                                                    y);
 }
 
 }  // namespace dnnk
